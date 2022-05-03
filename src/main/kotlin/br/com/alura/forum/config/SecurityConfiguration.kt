cf2882c78fc79f89790b9a617be4f718611b8ec9
Marlon <marlon.schemberger@gmail.com>
2022-05-03 20:01:35 -0300

    Autenticando com JWT Token

--- src/main/kotlin/br/com/alura/forum/config/SecurityConfiguration.kt
+++ src/main/kotlin/br/com/alura/forum/config/SecurityConfiguration.kt
@@ -1,7 +1,10 @@
 package br.com.alura.forum.config
 
+import br.com.alura.forum.security.JWTAuthenticatonFilter
+import br.com.alura.forum.security.JWTLoginFilter
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
@@ -9,30 +12,40 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+import org.springframework.web.filter.OncePerRequestFilter
 
 @Configuration
 @EnableWebSecurity
 class SecurityConfiguration(
-    private val userDetailsService: UserDetailsService
+    private val userDetailsService: UserDetailsService,
+    private val jwtUtil: JWTUtil
 ) : WebSecurityConfigurerAdapter() {
 
     override fun configure(http: HttpSecurity?) {
         //se o http n for nulo
-        http?.
+        http?.csrf()?.disable()?.
             //autoriza a requisição
-        authorizeRequests()?.
-            antMatchers("/topicos")?.hasAuthority("LEITURA_ESCRITA")?.
+        authorizeRequests()?.antMatchers("/topicos")?.hasAuthority("LEITURA_ESCRITA")?.
             //qualquer requisição
-        anyRequest()?.
+        antMatchers(HttpMethod.POST, "/login")?.permitAll()?.anyRequest()?.
             //tem que ser autenticada
         authenticated()?.
             //e não salve a sessao
-        and()?.sessionManagement()?.sessionCreationPolicy(SessionCreationPolicy.STATELESS)?.
-            //e desative o formulario
-        and()?.formLogin()?.disable()?.httpBasic()
+        and()
+        http?.addFilterBefore(
+            JWTLoginFilter(authManager = authenticationManager(), jwtUtil = jwtUtil),
+            UsernamePasswordAuthenticationFilter().javaClass
+        )
+        http?.addFilterBefore(
+            JWTAuthenticatonFilter(jwtUtil = jwtUtil),
+            UsernamePasswordAuthenticationFilter().javaClass
+        )
+        http?.sessionManagement()?.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
     }
+
     @Bean
-    fun bCryptPasswordEncoder(): BCryptPasswordEncoder{
+    fun bCryptPasswordEncoder(): BCryptPasswordEncoder {
         return BCryptPasswordEncoder()
     }
 
